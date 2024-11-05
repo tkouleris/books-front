97f1212d2684cec1b4dfbbfb1b8b5018208d447c
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-11-05 06:34:19 +0200

    admin lte template

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,20 @@
 function Footer() {
-    return ( <div>Footer</div>)
+    return <div>
+
+  <footer className="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div className="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0-rc
+    </div>
+  </footer>
+
+
+  <aside className="control-sidebar control-sidebar-dark">
+
+  </aside>
+
+    </div>
 }
 
 export default Footer;
