323a9045a5869643e3f15b07e43d5317d643b8d9
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-11-08 05:37:49 +0200

    registration

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -2,10 +2,14 @@ import {useNavigate} from "react-router-dom";
 import {useState} from "react";
 
 export default function RegistrationPage(){
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     // const [email, setEmail] = useState('')
     // const [username, setUsername] = useState('')
     // const [password, setPassword] = useState('')
+
+    function goToLogin() {
+        navigate("/login");
+    }
 
     return (
         <body className={"register-page bg-body-secondary"}>
@@ -14,7 +18,7 @@ export default function RegistrationPage(){
             <div className={"card"}>
                 <div className={"card-body register-card-body"}>
                     <p className={"register-box-msg"}>Register a new membership</p>
-                    <form action="../index3.html" method="post">
+
                         <div className="input-group mb-3"> <input type="text" className="form-control" placeholder="Full Name" />
                             <div className="input-group-text"> <span className="bi bi-person"></span> </div>
                         </div>
@@ -26,20 +30,16 @@ export default function RegistrationPage(){
                         </div>
                         <div className="row">
                             <div className="col-8">
-                                <div className="form-check"> <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /> <label className="form-check-label" for="flexCheckDefault" >
-                                    I agree to the <a href="#">terms</a> </label> </div>
+                                {/*<div className="form-check"> <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /> <label className="form-check-label" for="flexCheckDefault" >*/}
+                                {/*    I agree to the <a href="#">terms</a> </label> </div>*/}
                             </div>
                             <div className="col-4">
-                                <div className="d-grid gap-2"> <button type="submit" className="btn btn-primary">Sign In</button> </div>
+                                <div className="d-grid gap-2"> <button type="submit" className="btn btn-primary">Register</button> </div>
                             </div>
                         </div>
-                    </form>
-                    <div className="social-auth-links text-center mb-3 d-grid gap-2">
-                        <p>- OR -</p> <a href="#" className="btn btn-primary"> <i className="bi bi-facebook me-2"></i> Sign in using Facebook
-                    </a> <a href="#" className="btn btn-danger"> <i className="bi bi-google me-2"></i> Sign in using Google+
-                    </a>
-                    </div>
-                    <p className="mb-0"> <a href="login.html" className="text-center">
+
+
+                    <p className="mb-0"> <a onClick={goToLogin} href="" className="text-center">
                         I already have a membership
                     </a> </p>
                 </div>
