c16e99b068726586d3ff43d0373ecb1e5d0dfd05
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-11-11 06:39:38 +0200

    my books and my readings html

--- src/pages/MyBooks.jsx
+++ src/pages/MyBooks.jsx
@@ -1,5 +1,28 @@
+import Header from "../components/Header.jsx";
+import SideNav from "../components/SideNav.jsx";
+import Footer from "../components/Footer.jsx";
+
 function MyBooks(){
-    return <div>My Books</div>
+    return <div className="wrapper">
+        <Header/>
+        <div className="content-wrapper">
+            <div className="content-header">
+                <div className="container-fluid">
+                    <div className="row mb-2">
+                        <div className="col-sm-6">
+                            <h1 className="m-0">My Books</h1>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <section className="content">
+                My Books
+            </section>
+        </div>
+        <SideNav/>
+        <Footer/>
+    </div>;
 }
 
 export default MyBooks;
--- src/pages/MyReadings.jsx
+++ src/pages/MyReadings.jsx
@@ -1,5 +1,28 @@
+import Header from "../components/Header.jsx";
+import SideNav from "../components/SideNav.jsx";
+import Footer from "../components/Footer.jsx";
+
 function MyReadings(){
-    return <div>My Readings</div>
+    return <div className="wrapper">
+        <Header/>
+        <div className="content-wrapper">
+            <div className="content-header">
+                <div className="container-fluid">
+                    <div className="row mb-2">
+                        <div className="col-sm-6">
+                            <h1 className="m-0">My Readings</h1>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <section className="content">
+                My Readings
+            </section>
+        </div>
+        <SideNav/>
+        <Footer/>
+    </div>;
 }
 
 export default MyReadings
