8f2824df5119e5c5afb6ba8c1aec2bffcd6d788f
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-11-18 06:17:55 +0200

    config.example

--- config.example.jsx
+++ config.example.jsx
@@ -1,1 +1,5 @@
 export const login_info = { url: '', method: 'post'};
+export const register_info = { url: '', method: 'post'};
+export const dashboard_info = { url: '', method: 'post'};
+export const store_book = {url:'', method: 'post'};
+export const get_book = {url:'', method: 'get'};
