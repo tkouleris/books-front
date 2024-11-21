4551f997fadade3cd5abe51bfbde5a25696daa90
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-11-21 06:43:28 +0200

    reading form

--- src/pages/ReadingsForm.jsx
+++ src/pages/ReadingsForm.jsx
@@ -21,6 +21,10 @@ function ReadingsForm(){
         })
     }, []);
 
+    function handle(id){
+        console.log(id);
+    }
+
     return <div className="wrapper">
         <Header/>
         <div className="content-wrapper">
@@ -47,11 +51,14 @@ function ReadingsForm(){
                                     <div className="card-body">
                                         <div className="form-group">
                                             <label htmlFor="exampleSelectRounded0">Book </label>
-                                            <select className="custom-select rounded-0" id="exampleSelectRounded0">
+                                            <select className="custom-select rounded-0"
+                                                    id="slc_book"
+                                            >
                                                 {
                                                     books.map((book, index) => {
                                                         return <option key={book.id} value={book.id}
-                                                                       style={{backgroundImage: book.image}}>
+                                                                       style={{backgroundImage: book.image}}
+                                                        >
                                                             {book.title}
 
                                                         </option>
@@ -71,35 +78,7 @@ function ReadingsForm(){
                                                 <DatePicker selected={ended} onChange={(date) => setEnded(date)}/>
                                             </div>
                                         </div>
-                                        {/*<div className="form-group">*/}
-                                        {/*    <label>Started:</label>*/}
-                                        {/*    <div className="input-group date" id="reservationdate"*/}
-                                        {/*         data-target-input="nearest">*/}
-                                        {/*        <input type="text" className="form-control datetimepicker-input"*/}
-                                        {/*               data-target="#reservationdate"/>*/}
-                                        {/*        <div className="input-group-append" data-target="#reservationdate"*/}
-                                        {/*             data-toggle="datetimepicker">*/}
-                                        {/*            <div className="input-group-text"><i className="fa fa-calendar"></i>*/}
-                                        {/*            </div>*/}
-                                        {/*        </div>*/}
-                                        {/*    </div>*/}
-                                        {/*</div>*/}
-                                        {/*<div className="form-group">*/}
-                                        {/*    <label>Ended:</label>*/}
-                                        {/*    <div className="input-group date" id="reservationdate"*/}
-                                        {/*         data-target-input="nearest">*/}
-                                        {/*        <input type="text" className="form-control datetimepicker-input"*/}
-                                        {/*               data-target="#reservationdate"/>*/}
-                                        {/*        <div className="input-group-append" data-target="#reservationdate"*/}
-                                        {/*             data-toggle="datetimepicker">*/}
-                                        {/*            <div className="input-group-text"><i className="fa fa-calendar"></i>*/}
-                                        {/*            </div>*/}
-                                        {/*        </div>*/}
-                                        {/*    </div>*/}
-                                        {/*</div>*/}
                                     </div>
-
-
                                     <div className="card-footer">
                                         <button type="button" className="btn btn-primary">Save</button>
                                     </div>
