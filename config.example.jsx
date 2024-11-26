b1115cc5398a63e0cfe669d2a6769b1e117d84ed
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-11-26 06:12:17 +0200

    config

--- config.example.jsx
+++ config.example.jsx
@@ -6,3 +6,4 @@ export const get_book = {url:'', method: 'get'};
 export const get_books = {url:'', method: 'get'};
 export const delete_book = {url:'', method: 'delete'};
 export const store_reading = {url:'', method: 'post'};
+export const all_readings = {url:'', method: 'get'};
