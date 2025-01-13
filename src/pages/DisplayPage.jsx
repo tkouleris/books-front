49aa812cab5fc0072f61e57dec46a9c4b2e9508f
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-01-13 06:26:06 +0200

    display page - wip

--- src/pages/DisplayPage.jsx
+++ src/pages/DisplayPage.jsx
@@ -1,4 +1,12 @@
+import {useEffect} from "react";
+import {useParams} from "react-router-dom";
+
 function DisplayPage() {
+    let { username } = useParams();
+    useEffect(() => {
+        document.title = 'Display - ' + username;
+    }, []);
+
     return <div>
         Hello
     </div>
