bc9973747872fb2337c1f589a2dafa2847b9fdf9
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-01-13 05:58:02 +0200

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import PrivateRoutes from "./components/PrivateRoutes.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegistrationPage from "./pages/RegistrationPage.jsx";
-import Home from "./components/Home.jsx";
 import DashboardPage from "./pages/DashboardPage.jsx";
 import MyBooks from "./pages/MyBooks.jsx";
 import MyReadings from "./pages/MyReadings.jsx";
@@ -22,17 +21,14 @@ const router = createBrowserRouter([
             {
                 path: '/dashboard',
                 element: <DashboardPage />,
-                // errorElement: <NotFoundPage />
             },
             {
                 path: '/books',
                 element: <MyBooks />,
-                // errorElement: <NotFoundPage />
             },
             {
                 path: '/book',
                 element: <BookForm />,
-                // errorElement: <NotFoundPage />
             },
             {
                 path: '/book/:id',
@@ -42,56 +38,33 @@ const router = createBrowserRouter([
             {
                 path: '/readings',
                 element: <MyReadings />,
-                // errorElement: <NotFoundPage />
             },
             {
                 path: '/reading-form',
                 element: <ReadingsForm />,
-                // errorElement: <NotFoundPage />
             },
             {
                 path: '/reading-form/:id',
                 element: <ReadingsForm />,
-                // errorElement: <NotFoundPage />
             },
             {
                 path: '/profile',
                 element: <ProfilePage />,
-                // errorElement: <NotFoundPage />
             },
         ],
     },
     {
         path: '/login',
         element: <LoginPage />,
-        // errorElement: <NotFoundPage />
     },
     {
         path: '/register',
         element: <RegistrationPage />,
-        // errorElement: <NotFoundPage />
-    },
-    {
-        path: '/home',
-        element: <Home />,
-        // errorElement: <NotFoundPage />
     },
     {
         path: '/404',
         element: <NotFound />,
-        // errorElement: <NotFoundPage />
     },
-
-    // {
-    //     path: '/gameboard',
-    //     element: <GameBoardPage />,
-    //     // errorElement: <NotFoundPage />
-    // },
-    // {
-    //     path: '/results',
-    //     element: <RoundResultPage />,
-    //     // errorElement: <NotFoundPage />
-    // },
 ])
 
 createRoot(document.getElementById('root')).render(
