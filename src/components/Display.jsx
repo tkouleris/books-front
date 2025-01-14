acacb28b40005a3aed7b29f1a6c0a31714539590
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-01-14 06:34:32 +0200

    display page

---
+++ src/components/Display.jsx
@@ -0,0 +1,29 @@
+function Display({reading}) {
+    return <div key={reading.id} className="col-md-2" style={{margin: "5px 5px", border: '1px solid black'}}>
+        <div className="row">
+            <div className="col-12" style={{
+                textAlign: "center",
+                backgroundColor: "#0096FF",
+                paddingTop: 0,
+                paddingBottom: 0
+            }}>
+                <b>{reading.book.title}</b>
+            </div>
+            <div className="col-6" style={{paddingLeft: 0}}>
+                <img alt={reading.book.title} style={{width: '100%', height: 200}}
+                     src={reading.book.image}/>
+            </div>
+            <div className="col-6">
+                {reading.book.description}
+            </div>
+            <div className="col-12" style={{textAlign: "center"}}>
+                <b>Started:</b> <span>{reading.started}</span>
+            </div>
+            <div className="col-12" style={{textAlign: "center"}}>
+                <b>Ended:</b> <span>{reading.ended}</span>
+            </div>
+        </div>
+    </div>
+}
+
+export default Display;
