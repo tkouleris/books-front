c52ba013b75a741d2ebcde30385c8367000337cd
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-01-15 05:54:32 +0200

    display page

--- src/pages/DisplayPage.jsx
+++ src/pages/DisplayPage.jsx
@@ -26,22 +26,21 @@ function DisplayPage() {
                     <div className="row mb-1">
                         <div className="col-sm-6">
                             <h1 className="m-0">My Latest Reads</h1>
+                            <div className="row">
+                                <div className="col-sm-2">
+                                    <img src={user.avatar} style={{width: 150, height: 150}}/>
+                                </div>
+                                <div className="col-sm-2">
+                                    <b>Username: </b>{user.username}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <section className="content">
                 <div className="container-fluid">
-                    <div  style={{marginBottom: 20, border: '1px solid black', width: 460}}>
-                        <div className="row">
-                            <div className="col-4">
-                                <img src={user.avatar} style={{width: 150, height: 150}}/>
-                            </div>
-                            <div className="col-1" style={{textAlign: "left"}}>
-                                <div><b>Username: </b>{user.username}</div>
-                                <div><b>Email: </b>{user.email}</div>
-                            </div>
-                        </div>
+                    <div className="row" style={{borderBottom: "1px solid black", padding: "10px"}}>
 
                     </div>
                     <div className="row">
@@ -60,11 +59,13 @@ function DisplayPage() {
                     </div>
                 </div>
             </section>
+            <div className="row" style={{borderBottom: "1px solid black", padding: "10px"}}>
 
+            </div>
             <section className="content mt-3">
                 <div className="container-fluid">
 
-                <div className="row">
+                    <div className="row">
                         <div className="col-sm-12">
                             <h3>Latest Reads</h3>
                         </div>
