2ed9b156232cdc3509a603087f8d662777609153
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-01-31 06:39:00 +0200

    book search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,27 +15,27 @@ function Header() {
             </ul>
 
             <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <a className="nav-link" data-widget="navbar-search" href="#" role="button">
-                        <i className="fas fa-search"></i>
-                    </a>
-                    <div className="navbar-search-block">
-                        <form className="form-inline">
-                            <div className="input-group input-group-sm">
-                                <input className="form-control form-control-navbar" type="search" placeholder="Search"
-                                       aria-label="Search"/>
-                                <div className="input-group-append">
-                                    <button className="btn btn-navbar" type="submit">
-                                        <i className="fas fa-search"></i>
-                                    </button>
-                                    <button className="btn btn-navbar" type="button" data-widget="navbar-search">
-                                        <i className="fas fa-times"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </li>
+                {/*<li className="nav-item">*/}
+                {/*    <a className="nav-link" data-widget="navbar-search" href="#" role="button">*/}
+                {/*        <i className="fas fa-search"></i>*/}
+                {/*    </a>*/}
+                {/*    <div className="navbar-search-block">*/}
+                {/*        <form className="form-inline">*/}
+                {/*            <div className="input-group input-group-sm">*/}
+                {/*                <input className="form-control form-control-navbar" type="search" placeholder="Search"*/}
+                {/*                       aria-label="Search"/>*/}
+                {/*                <div className="input-group-append">*/}
+                {/*                    <button className="btn btn-navbar" type="submit">*/}
+                {/*                        <i className="fas fa-search"></i>*/}
+                {/*                    </button>*/}
+                {/*                    <button className="btn btn-navbar" type="button" data-widget="navbar-search">*/}
+                {/*                        <i className="fas fa-times"></i>*/}
+                {/*                    </button>*/}
+                {/*                </div>*/}
+                {/*            </div>*/}
+                {/*        </form>*/}
+                {/*    </div>*/}
+                {/*</li>*/}
 
 
                 {/*<li className="nav-item dropdown">*/}
