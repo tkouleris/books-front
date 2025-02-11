38c5367b0c6b810e473269ec87a203188ab1a6b1
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-02-11 06:12:13 +0200

    config

--- config.example.jsx
+++ config.example.jsx
@@ -14,3 +14,4 @@ export const store_profile = {url:'', method: 'post'};
 export const display_data = {url:'', method: 'get'};
 export const to_read_list = {url:'', method: 'get'};
 export const remove_from_red_list = {url:'', method: 'delete'};
+export const add_to_red_list = {url:'', method: 'post'};
