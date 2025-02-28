93775d422f4bf47d3183985d3e00d3e2eb410d9a
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-02-28 03:23:12 +0200

    config

--- src/pages/UserVerificationPage.jsx
+++ src/pages/UserVerificationPage.jsx
@@ -1,9 +1,35 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function UserVerificationPage() {
     let { token } = useParams();
+    // const [msg, setMsg] = useState('')
 
-    return <div>
-        User Verification Page {token}
+    // useEffect(() => {
+    //
+    // }, []);
+
+
+    return <div className="content-wrapper" style={{marginLeft: 40}}>
+        <div className="content-header">
+            <div className="container-fluid">
+                <div className="row mb-2">
+                    <div className="col-sm-6">
+                        <h1 className="m-0">Verification Page</h1>
+                    </div>
+                    <div className="col-sm-6">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="content">
+            <div className="container-fluid">
+                <div className="row">
+
+                </div>
+            </div>
+        </div>
     </div>
 }
