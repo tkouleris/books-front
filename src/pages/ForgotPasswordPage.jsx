2ab4af0314e3c56bd86c0feda9781daa58033f8a
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-03-10 06:21:38 +0200

    forgot password - wip

--- src/pages/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage.jsx
@@ -1,5 +1,49 @@
+import {useEffect} from "react";
+
 function ForgotPasswordPage() {
-    return <div>forgot password</div>
+    useEffect(() => {
+        document.title = 'My Books - Forgot Password';
+        let bodyElement = document.getElementsByTagName('body')[0];
+        bodyElement.className = "hold-transition login-page";
+
+    }, []);
+
+    return <div className="login-box">
+        <div className="login-logo">
+            <a href="../../index2.html"><b>Admin</b>LTE</a>
+        </div>
+
+        <div className="card">
+            <div className="card-body login-card-body">
+                <p className="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
+
+                <form action="recover-password.html" method="post">
+                    <div className="input-group mb-3">
+                        <input type="email" className="form-control" placeholder="Email"/>
+                        <div className="input-group-append">
+                            <div className="input-group-text">
+                                <span className="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <button type="submit" className="btn btn-primary btn-block">Request new password</button>
+                        </div>
+
+                    </div>
+                </form>
+
+                <p className="mt-3 mb-1">
+                    <a href="login.html">Login</a>
+                </p>
+                <p className="mb-0">
+                    <a href="register.html" className="text-center">Register a new membership</a>
+                </p>
+            </div>
+
+        </div>
+    </div>
 }
 
 export default ForgotPasswordPage
