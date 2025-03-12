1cf3f0939ce8cc8ed583b996ee65088e8d796f26
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-03-12 05:31:12 +0200

    password change

--- src/pages/RecoverPasswordPage.jsx
+++ src/pages/RecoverPasswordPage.jsx
@@ -20,7 +20,24 @@ function RecoverPasswordPage() {
         navigate("/login");
     }
 
+    function checkPassword(str){
+        let re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        return re.test(str);
+    }
+
     function handlePasswordChange() {
+        let errorMsg = ''
+        if(!checkPassword(password)){
+            errorMsg += "- Password must be minimum 8 characters, with at least a symbol, upper and lower case letters and a number\r\n"
+        }
+        if(password !== confirmPassword){
+            errorMsg += "- Passwords do not match\r\n";
+        }
+
+        if(errorMsg.length > 0){
+            alert(errorMsg)
+            return false;
+        }
 
     }
 
@@ -34,34 +51,34 @@ function RecoverPasswordPage() {
                 <p className="login-box-msg">You are only one step a way from your new password, recover your password
                     now.</p>
 
-                <form action="login.html" method="post">
-                    <div className="input-group mb-3">
-                        <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)}
-                               placeholder="Password"/>
-                        <div className="input-group-append">
-                            <div className="input-group-text">
-                                <span className="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="password" className="form-control"
-                               onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm Password"/>
-                        <div className="input-group-append">
-                            <div className="input-group-text">
-                                <span className="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12">
-                            <button type="submit" onClick={handlePasswordChange}
-                                    className="btn btn-primary btn-block">Change password
-                            </button>
-                        </div>
 
+                <div className="input-group mb-3">
+                    <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)}
+                           placeholder="Password"/>
+                    <div className="input-group-append">
+                        <div className="input-group-text">
+                            <span className="fas fa-lock"></span>
+                        </div>
                     </div>
-                </form>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="password" className="form-control"
+                           onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm Password"/>
+                    <div className="input-group-append">
+                        <div className="input-group-text">
+                            <span className="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12">
+                        <button type="submit" onClick={handlePasswordChange}
+                                className="btn btn-primary btn-block">Change password
+                        </button>
+                    </div>
+
+                </div>
+
 
                 <p className="mt-3 mb-1">
                     <a href="login.html">Login</a>
