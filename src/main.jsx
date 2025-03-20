61b67ba8473f1e1c22c69bfe002ffd3ccc679a67
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-03-20 05:41:15 +0200

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 import PrivateRoutes from "./components/PrivateRoutes.jsx";
@@ -92,8 +92,8 @@ const router = createBrowserRouter([
     },
 ])
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
       <RouterProvider router={router} />
-  </StrictMode>,
+    </React.StrictMode>,
 )
